619f403eca6ed6c554daa1de5a6e13e21d48080d
Nikolas-vl <nikvv02@gmail.com>
2025-03-07 21:55:52 +0100

    fixed bg color and bnf

--- src/js/benefits.js
+++
@@ -1,6 +0,0 @@
-const bnfBtn = document.querySelector('#bnfBtn');
-const scrollFooter = document.querySelector('#footer');
-
-bnfBtn.addEventListener('click', () => {
-  scrollFooter.scrollIntoView({ behavior: 'smooth' });
-});
